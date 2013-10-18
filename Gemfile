ccaa918db95216a189e4ce54ec7706fd7a493c2e
José Alberto Pérez Melián <japmelian@gmail.com>
2013-10-18 11:11:40 +0100

    Copiando archivos de p03_haml

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem "haml"
+
+group :development do
+	gem "rack", "~>1.5.2"
+	gem "pry-debugger", "~> 0.2.2"
+	gem "thin", "~> 1.5.1"
+end
---
+++ rock_paper.rb
@@ -0,0 +1,58 @@
+require 'rack/request'
+require 'rack/response'
+require 'haml'
+  
+module RockPaperScissors
+  class App 
+
+    def initialize(app = nil)
+      @app = app
+      @content_type = :html
+      # Posibles jugadas
+      @defeat = {'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper'}
+      # Posibles tiradas
+      @throws = @defeat.keys
+      
+    end
+
+    def haml(template, resultado)
+      template_file = File.open(template, 'r')
+      Haml::Engine.new(File.read(template_file)).render({},resultado)
+    end
+
+    def call(env)
+      req = Rack::Request.new(env)
+
+      req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
+
+      # El ordenador tira (al azar)
+      computer_throw = @throws.sample
+      # El Jugador escoge (por medio de botones)
+      player_throw = req.GET["choice"] # del path
+
+      anwser = 
+        if (player_throw == computer_throw && (player_throw != '' && computer_throw != ''))
+          "TIE"
+        elsif computer_throw == @defeat[player_throw]
+          "WIN"
+        else
+          "LOSE"
+        end
+	
+  # Hash con info
+	resultado = 
+		{
+		:choose => @choose,
+		:anwser => anwser,
+		:throws => @throws,
+		:computer_throw => computer_throw,
+		:player_throw => player_throw
+    }
+
+    res = Rack::Response.new(haml("views/index.html.haml", resultado))
+     
+    end # call
+  end   # App
+end     # RockPaperScissors
+  
+
